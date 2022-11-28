18dbd82c91779cda154be47369b6688028876488
Mohamed El hadjaoui <mel-hadj@e1r9p5.1337.ma>
2022-11-28 13:31:54 +0100

    bismillah

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from './component/Header'
+
+function App() {
+
+  return (
+    <div className='App'>
+     <Header></Header>
+    </div>
+  )
+}
+
+export default App
