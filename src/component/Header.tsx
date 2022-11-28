d0ffb0eb82d83b5d39933894dcc754925e9ec953
Mohamed El hadjaoui <mel-hadj@e1r9p5.1337.ma>
2022-11-28 17:17:47 +0100

    working on navbar

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { MagnifyingGlassIcon, CpuChipIcon, UserCircleIcon, BeakerIcon, BellIcon } from '@heroicons/react/24/outline';
 
 function Header() {
   return (
-    <div>Hello</div>
+    /*NavBar*/
+    <div className='bg-[#F0F5F5] w-full py-6 items-center justify-between flex px-12  '>
+      {/*Input*/}
+      <div className='w-full lg:flex hidden space-x-4 items-center justify-start py-2'>
+        <MagnifyingGlassIcon className='w-4 h-4'></MagnifyingGlassIcon>
+        <input type="text" placeholder='Search anything' className='bg-transparent outline-none' />
+      </div>
+      {/*Logo*/}
+      <div className='w-full flex  space-x-4 items-center justify-center py-2'> 
+        <CpuChipIcon className='w-6 h-6'></CpuChipIcon>
+        <h1 className='text-xl text-gray-900 font-medium'  >Dashboard</h1>
+      </div>
+      {/*Icons*/}
+      <div className='w-full flex  space-x-4 items-center justify-end py-2'>
+        <button>
+        <BellIcon  className='w-6 h-6'>
+        </BellIcon>
+        </button>
+        <UserCircleIcon  className='w-6 h-6'>
+          <button></button>
+        </UserCircleIcon>
+      </div>
+    </div>
   )
 }
 
 export default Header
+
+
+
